e613c4e4e2bbf30117fa97169f238d862a97ede2
Hieu Tran <hieutran106@gmail.com>
2018-03-19 09:34:16 +1000

    add highlight layer for possible moves

--- src/app/core/chessBoard.component.ts
+++ src/app/core/chessBoard.component.ts
@@ -6,11 +6,21 @@ import { Component } from '@angular/core';
 })
 export class ChessBoardComponent {
     chessBoard: ChessBoard;
+    possibleMovesMap:number[];
     constructor() {
         this.chessBoard=new ChessBoard();
-        
+        this.possibleMovesMap = Array(64).fill(0);
+        this.possibleMovesMap[0]=1;
+        this.possibleMovesMap[12]=1;
+        this.possibleMovesMap[13]=1;
+    }
+    getPossibleMovesClasses(index:number):string {
+        if (this.possibleMovesMap[index]!=0) {
+            return "possibleMoveEmpty";
+        } else return "";
     }
     clickAtIndex(index:number) {
+        this.possibleMovesMap[index]=1;
         console.log(index);
     }
 }
